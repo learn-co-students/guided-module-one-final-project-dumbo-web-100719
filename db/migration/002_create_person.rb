3509de653fa15bf195ff3412ff7a1eed7f5359c6
Hamidou Diallo <Dio.hamidou@gmail.com>
2019-10-22 14:13:16 -0400

    tables working, migration worked, seeding is working

--- db/migration/002_create_person.rb
+++
@@ -1,8 +0,0 @@
-class CreatePerson < ActiveRecord::Migrate[5.2]
-  def change
-    create_table :persons do
-      t.string :name
-      t.integer :age 
-    end
-  end
-end
---
+++ lib/methods.rb
@@ -0,0 +1,68 @@
+# require "tty-prompt"
+# # require_relative './models/Person.rb'
+
+# # 1.PUTS “Welcome to MoodHacker”
+# # Puts “Welcome to MoodHacker”
+# # Puts “Take charge of your life by looking inward”
+
+# def welcome
+#   puts "Welcome to MoodHacker!"
+#   puts "Take charge of your life by looking inward."
+# end
+# def name_age_input
+#   prompt = TTY::Prompt.new
+#   person_name = prompt.ask("What's your name?")
+#   Person.create(name: person_name)
+# end
+
+# def display_emoticon
+# prompt = TTY::Prompt.new
+#   # prompt_choice = prompt.select(“How are you feeling today?“, %w(URL- gifhappy URL - gif_bad?))
+# prompt_choice = prompt.select("How are you feeling today", %w(Soft_taco Hard_taco 
+# Porque_no_los_dos?))
+#   prompt_choice
+# end
+
+# welcome
+# name_age_input
+# display_emoticon
+
+# # P display_emoticon
+# # 2.DISPLAY happy face | sad face
+# # prompt.select(“How are you feeling today?“, %w(URL- gifhappy URL - gif_bad?))
+# # 3. Display options:
+# def emotion_list
+#   # if display_emoticon == ""
+# end
+# # Happy emotions:
+# # Excited
+# # Grateful
+# # Inspired
+# # Loved
+# # Calm
+# # Sad Emotions:
+# # Depressed
+# # Lonely
+# # Disappointed
+# # Jealous
+# # Hungry
+# # Anxious
+# # 4. Intensity (1-5)
+# # Gifs -- 3 for each
+# # 5. Why?
+# # Categories:
+# # Love
+# # Career
+# # Family
+# # Health
+# # Social
+# # 6. Describe the event (logs with timestamp)
+# # And log date (maybe)
+# # 7. Displays log (and log history)
+# # Display most frequent emotions with prescription
+
+
+
+
+
+
