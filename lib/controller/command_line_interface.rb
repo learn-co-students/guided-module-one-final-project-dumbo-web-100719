6cb7156b694af8119d002c84c4a8fee18516727d
Hamidou Diallo <Dio.hamidou@gmail.com>
2019-10-23 11:32:00 -0400

    drop down menu working

--- lib/controller/command_line_interface.rb
+++ lib/controller/command_line_interface.rb
@@ -1,5 +1,6 @@
 require 'tty-prompt'
 require 'pry'
+
 class CommandLineInterface
   def welcome
     puts "Welcome to MoodHacker!"
@@ -15,11 +16,30 @@ class CommandLineInterface
   def display_emoji_choices
     prompt = TTY::Prompt.new
     mood = prompt.select("How are you feeling today, #{User.last.name}?") do |menu|
-   menu.choice 'Good'
-   menu.choice 'Not so great'
-    mood
+    menu.choice 'Good'
+    menu.choice 'Not so great'
+    end
+    # mood
+    
+    if mood == 'Good' then prompt.select("What is your feeling?") do |menu|
+      menu.choice 'Excited'
+      menu.choice 'Grateful'
+      menu.choice 'Inspired'
+      menu.choice 'Loved'
+      menu.choice 'Calm'
+    end
+    elsif mood == 'Not so great' then prompt.select("What is your feeling?") do |menu|
+      menu.choice 'Depressed'
+      menu.choice 'Lonely'
+      menu.choice 'Disappointed'
+      menu.choice 'Jealous'
+      menu.choice 'Hungry'
+      menu.choice 'Anxious'
+      end
     end
   end
 
+
 end
 
+# 'Not so great' then prompt.select("What is your feeling?") do |menu|
