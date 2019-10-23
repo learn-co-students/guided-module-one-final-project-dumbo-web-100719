02f428be58c43c93dd64c9a5b29df5fe27490b09
Bri Fetterley <bnfetterley@usfca.edu>
2019-10-23 16:56:49 -0400

    changes

--- bin/run.rb
+++ bin/run.rb
@@ -5,6 +5,6 @@ require_relative '../config/environment'
 new_cli = CommandLineInterface.new
 new_cli.welcome
 new_cli.get_name
-new_cli.display_emoji_choices
+new_cli.feeling_attributes
 new_cli.event_categories
-new_cli.describe_event
+new_cli.create_feeling
--- lib/controller/command_line_interface.rb
+++ lib/controller/command_line_interface.rb
@@ -9,26 +9,26 @@ class CommandLineInterface
 
   def get_name
     prompt = TTY::Prompt.new
-    name = prompt.ask("What is your name?")
-    User.create(name: name)
+    @name = prompt.ask("What is your name?")
+    @user = User.create(name: @name)
   end
 
-  def display_emoji_choices
+  def feeling_attributes
     prompt = TTY::Prompt.new
-    mood = prompt.select("How are you feeling today, #{User.last.name}?") do |menu|
+    @mood = prompt.select("How are you feeling today, #{@user.name}?") do |menu|
+    # binding.pry
     menu.choice 'Good'
     menu.choice 'Not so great'
     end
-    # mood
     
-    if mood == 'Good' then feeling = prompt.select("What is your feeling?") do |menu|
+    if @mood == 'Good' then @feeling = prompt.select("What is your feeling?") do |menu|
       menu.choice 'Excited'
       menu.choice 'Grateful'
       menu.choice 'Inspired'
       menu.choice 'Loved'
       menu.choice 'Calm'
     end
-    elsif mood == 'Not so great' then feeling = prompt.select("What is your feeling?") do |menu|
+    elsif @mood == 'Not so great' then @feeling = prompt.select("What is your feeling?") do |menu|
       menu.choice 'Depressed'
       menu.choice 'Lonely'
       menu.choice 'Disappointed'
@@ -37,31 +37,19 @@ class CommandLineInterface
       menu.choice 'Anxious'
       end
     end
-    feeling_intensity = prompt.select("How strong is this feeling on a scale of 1-5 (5 being the strongest)?") do |menu|
+    @feeling_intensity = prompt.select("How strong is this feeling on a scale of 1-5 (5 being the strongest)?") do |menu|
       menu.choice '1'
       menu.choice '2'
       menu.choice '3'
       menu.choice '4'
       menu.choice '5'
     end
-    Feeling.create(name: feeling,user_id: User.all.last.id, intensity: feeling_intensity)
+    @feeling_instance = Feeling.create(name: @feeling,user_id: @user.id , intensity: @feeling_intensity)
   end
-
-#   def intensity
-#     prompt = TTY::Prompt.new
-#     feeling_intensity = prompt.select("How strong is this feeling on a scale of 1-5 (5 being the strongest)?") do |menu|
-#     menu.choice '1'
-#     menu.choice '2'
-#     menu.choice '3'
-#     menu.choice '4'
-#     menu.choice '5'
-#   end
-  
-# end
 
 def event_categories
   prompt = TTY::Prompt.new
-  event_category = prompt.select("What part of your life is this related to?") do |menu|
+  @event_category = prompt.select("What part of your life is this related to?") do |menu|
   menu.choice 'Love'
   menu.choice 'Career'
   menu.choice 'Family'
@@ -70,8 +58,11 @@ def event_categories
 end
 event_description = prompt.ask("What happened?")
 event_description
-Event.create(category: event_category,description: event_description)
-# Feeling.update_column(:event_id, Event.all.last.id)
+@event = Event.create(category: @event_category,description: @event_description)
+end
+
+def create_feeling
+  Feeling.create(name: @feeling, intensity: @feeling_intensity, user_id: @user.id, event_id: @event.id)
 end
 
 # def describe_event
